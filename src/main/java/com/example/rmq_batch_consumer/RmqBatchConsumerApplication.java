570dbe8c375044d29ed708b79f41ab2c0d857a57
panchaliSam <panchalisam20030117@gmail.com>
2025-07-31 14:26:36 +0530

    Initial commit

---
+++ src/main/java/com/example/rmq_batch_consumer/RmqBatchConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rmq_batch_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmqBatchConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmqBatchConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rmq_batch_consumer/RmqBatchConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rmq_batch_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmqBatchConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
